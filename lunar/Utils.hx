27e66740ced6adaaedd2c034f2985168c28a1647
R32 <hotup@qq.com>
2017-04-28 00:24:05 +0800

    remove unused code

--- lunar/Utils.hx
+++ lunar/Utils.hx
@@ -21,34 +21,3 @@ class Utils {
 		return bits;
 	}
 }
-
-/**
-当想要检索某一个小范围的正整数是否属于某一集合时（未测试和使用）
-*/
-abstract IntColl(haxe.io.Bytes) {
-
-	public inline function new(max: Int) this = haxe.io.Bytes.alloc(max);
-	public inline function set(i: Int) this.set(i, 1);
-	public inline function unset(i: Int) this.set(i, 0);
-	public inline function get(i: Int) return haxe.io.Bytes.fastGet(this.getData(), i);
-	public inline function has(i: Int) return get(i) == 1;
-
-	public var length(get, never):Int;
-	public function get_length() {
-		var b = this.getData();
-		var c = 0;
-		for (i in 0...this.length) {
-			if (haxe.io.Bytes.fastGet(b, i) == 1) ++c;
-		}
-		return c;
-	}
-
-	public function toArray():Array<Int> {
-		var a = [];
-		var b = this.getData();
-		for (i in 0...this.length) {
-			if (haxe.io.Bytes.fastGet(b, i) == 1) a.push(i);
-		}
-		return a;
-	}
-}
